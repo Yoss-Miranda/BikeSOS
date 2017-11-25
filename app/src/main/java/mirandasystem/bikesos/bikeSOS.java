a4f29d827a368a60846144cbeb6f174cd168ced0
Yoss-Miranda <ed_yos@hotmail.com>
2017-11-24 22:52:58 -0600

    Imagen del boton de mapa e imagen del boton  Encendido agregado ya envia sms

--- app/src/main/java/mirandasystem/bikesos/bikeSOS.java
+++ app/src/main/java/mirandasystem/bikesos/bikeSOS.java
@@ -1,10 +1,14 @@
 package mirandasystem.bikesos;
 
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.telephony.SmsManager;
@@ -12,9 +16,11 @@ import android.view.MenuItem;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class bikeSOS extends AppCompatActivity{
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS =0 ;
     private String nombre,telefono,mensaje;
     private TextView nombre_label,telefono_label;
 
@@ -45,11 +51,35 @@ public class bikeSOS extends AppCompatActivity{
         startActivity(activitySettings);
     }
 
-    private void enviaSMS(){
+    public void sendSMS(View view){
+        sendSMSMessage();
 
-            SmsManager sms = SmsManager.getDefault();
-            sms.sendTextMessage(telefono, null,mensaje, null, null);
     }
+    protected void sendSMSMessage() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.SEND_SMS)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.SEND_SMS},
+                        MY_PERMISSIONS_REQUEST_SEND_SMS);
+            }
+        }
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(telefono, null, mensaje, null, null);
+            Toast.makeText(getApplicationContext(), "¡Mensaje Enviado!",
+                    Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(),
+                    "¡No se pudo enviar el SMS!",
+                    Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+    }
+
+
 
     private void cancelAlarma(){
 
@@ -73,9 +103,29 @@ public class bikeSOS extends AppCompatActivity{
         if (id == R.id.action_settings) {
             Intent activitySettings = new Intent(this, preferencias.class );
             startActivity(activitySettings);
+            finish();
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage(telefono, null, mensaje, null, null);
+                    Toast.makeText(getApplicationContext(), "Mensaje Enviado",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "Fallo al enviar SMS", Toast.LENGTH_LONG).show();
+                    return;
+                }
+            }
+        }
+
     }
 
 
